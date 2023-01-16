41026da5846c0384a7d15c20442a2566eedc9edd
dzmitry-kavalenka <dzmitriy.kavalenka@gmail.com>
2023-01-16 21:15:59 +0100

    fix: add 404 check for controller

--- src/routing.ts
+++ src/routing.ts
@@ -10,12 +10,14 @@ const router = (req: IncomingMessage, res: ServerResponse) => {
   const url = req.url.replace('/api', '');
   const method = req.method;
 
+  console.log({ url });
+
   const baseRoute = url.split('/')[1];
   const controller = routing[baseRoute];
 
   if (!controller) {
     res.statusCode = 404;
-    return;
+    res.end('Resource not found');
   }
 
   controller.forEach((handler, key) => {
@@ -24,13 +26,19 @@ const router = (req: IncomingMessage, res: ServerResponse) => {
       (key.path === url || url.split('/').length === key.path.split('/').length)
     ) {
       try {
+        console.log('handler');
         handler(req, res);
       } catch (error) {
         res.statusCode = 500;
         res.end('Unexpected server error');
       }
+    } else {
+      res.statusCode = 404;
+      res.end('Resource not found');
     }
   });
+
+  return;
 };
 
 export default router;
